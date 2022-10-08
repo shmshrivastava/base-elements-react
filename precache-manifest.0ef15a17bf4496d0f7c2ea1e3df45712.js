2b82ad5c1e9c7d70b8cf14dfdde4d320dc6ff407
Shivam Shrivastava <shivam270295@gmail.com>
2022-10-08 13:35:46 +0530

    Updates

---
+++ precache-manifest.0ef15a17bf4496d0f7c2ea1e3df45712.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ef8af1dd93e51b44df38c841b26077",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5e9e92f374c73985a496",
+    "url": "./static/css/main.7690fe4d.chunk.css"
+  },
+  {
+    "revision": "b850b997b1d32c7c26b4",
+    "url": "./static/js/2.9417d746.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "./static/js/2.9417d746.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e9e92f374c73985a496",
+    "url": "./static/js/main.cc507376.chunk.js"
+  },
+  {
+    "revision": "c3aa6f2e2dc3a7dcf083",
+    "url": "./static/js/runtime-main.9abda915.js"
+  },
+  {
+    "revision": "be8ba26026bf77fcb36a5352e1e037ea",
+    "url": "./static/media/star.be8ba260.svg"
+  }
+]);
